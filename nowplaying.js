783b2fce4a476618e3f2bf3c50b048913260c8fd
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2020-12-05 07:42:57 -0800

    Create nowplaying.js

---
+++ nowplaying.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "nowplaying",
+  aliases: ["np"],
+  code: `
+$channelSendMessage[$channelID;{color:RANDOM}{description: Now Playing $songInfo[title] - $songInfo[duration]s}]
+$onlyIf[$voiceID!=;{color:RED}{description: Please join a vc first}]
+$onlyIf[$queueLength>0;{color:RED}{description: Queue is empty}]`
+};
