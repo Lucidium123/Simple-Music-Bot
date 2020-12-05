aa0530b7ce34acd5e64176a5390473c85ce961c2
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2020-12-05 07:49:56 -0800

    Create queue.js

---
+++ queue.js
@@ -0,0 +1,8 @@
+  name: "queue",
+  aliases: ["qu"],
+  code: `
+$pauseSong
+$onlyIf[$voiceID!=;{color:RED}{description: Please join a Voice Channel First}]
+$onlyIf[$queueLength>0;{color:RED}{description: Queue is empty!!}]
+$addCmdReactions[⏸️]`
+};
